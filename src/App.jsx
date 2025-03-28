60f17fe5c8d3c305454427d1b2f87722fa28783a
G-FelixLima <rm95467@estudante.fieb.edu.br>
2025-03-28 09:45:22 -0300

    projeto pizzaria

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function App() {
+
+  const [nome, setNome] = useState("Memphis")
+
+  //var nome = "Zacarias Jr.";
+
+  return (
+    <div>
+        <h3>Pizzaria 2F</h3>
+        <input 
+        className="nome"
+        name = "nome"
+        onChange={(e)=>{setNome(e.target.value)}}
+        placeholder="Digite um nome..."
+        type="text" />
+        <button 
+        onClick={()=>{alert(nome)}}
+        className="botao">
+          CLIQUE AQUI
+        </button>
+    </div>
+  )
+}
+
+export default App
