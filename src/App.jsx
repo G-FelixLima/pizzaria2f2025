fc33c951984d11208b894dbedc666137d362f8e0
G-FelixLima <rm95467@estudante.fieb.edu.br>
2025-04-04 09:55:16 -0300

    Componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import { useState } from "react";
+import poke from './assets/pokeapi_256.png'
 
 function App() {
 
-  const [nome, setNome] = useState("Memphis")
-
   //var nome = "Zacarias Jr.";
 
-  const Fomulario = () => {
+  const Fomulario = (props) => {
+
+    const [nome, setNome] = useState("Memphis")
+
     return (
       <div>
+
         <input 
         className="nome"
-        name = "nome"
+        name = "nome"        
         onChange={(e)=>{setNome(e.target.value)}}
-        placeholder="Digite um nome..."
+        placeholder={props.sombra != null ? props.sombra : "Texto padrão"}
         type="text" />
+
         <button 
         onClick={()=>{alert(nome)}}
         className="botao">
           CLIQUE AQUI
         </button>
+
       </div>
     )
   }
@@ -27,6 +32,12 @@ function App() {
   return (
     <div>
         <h3>Pizzaria 2F</h3>
+        <p>Texto do parágrafo</p>
+        <img src={poke} style={{width:200, height:100}} />
+        <img src={poke} style={{width:200, height:100}} />
+        <Fomulario sombra="Digite seu nome..."/>
+        <Fomulario sombra="Digite seu email..."/>
+        <Fomulario sombra="(99) 90000-0000"/>
         <Fomulario />
         <Fomulario />
         <Fomulario />
